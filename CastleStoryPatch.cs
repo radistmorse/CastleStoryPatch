49f87a992e7feecf1b135538f147ded9608da6df
morse <morse@MORSECOMP>
2024-02-04 14:57:38 +0100

    Add project files.

---
+++ CastleStoryPatch.cs
@@ -0,0 +1,58 @@
+﻿using Brix.Lifecycle.Pooling;
+using Brix.UI.Scripts;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Doorstop
+{
+    class Entrypoint
+    {
+        public static void Start()
+        {
+            new Thread(() =>
+            {
+                Thread.Sleep(3000);
+
+                var harmony = new Harmony("CastleStoryPatch");
+
+                harmony.PatchAll();
+            }).Start();
+        }
+    }
+}
+
+namespace CastleStoryPatch
+{
+    [HarmonyPatch(typeof(CanvasScalerDriver))]
+    public class CanvasScalerDriver_Patch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("OnSetSettingIntoGame")]
+        public static bool OnSetSettingIntoGame(CanvasScalerDriver __instance)
+        {
+            var canvasScalers = Traverse.Create(__instance).Field("CanvasScalers").GetValue<List<CanvasScaler>>();
+            foreach (CanvasScaler canvasScaler in canvasScalers)
+            {
+                canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+                canvasScaler.referenceResolution = new Vector2(1920f, 1080f);
+            }
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(ObjectPoolSingleton))]
+    public class ObjectPoolSingleton_Patch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("ResetTransform")]
+        public static void ResetTransform(GameObject go)
+        {
+            go.transform.localScale = Vector3.one;
+        }
+    }
+}
